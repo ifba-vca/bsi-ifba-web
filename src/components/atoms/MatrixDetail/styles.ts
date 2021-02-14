f3f420f9a950774e27fc20b29389ed6da0f9dd48
cancio <filipe.cancio@outlook.com>
2021-02-14 17:33:28 -0300

    Configurando o modal

--- src/components/atoms/MatrixDetail/styles.ts
+++ src/components/atoms/MatrixDetail/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {_ubuh2,_ubuh3,_ubutextlittle} from '../../../Styles/generic/Typography';
 
 export const Container = styled.div`
   background-color: var(--si-background-02);
@@ -7,9 +8,11 @@ export const Container = styled.div`
   overflow: hidden;
   border-radius: 10px;
   width: 90vw;
-  height: 90vh;
-  left: 5vw;
-  top: 5vh;
+  max-width: 600px;
+  height: 400px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   transition: 1s;
   z-index: 10;
   &.show{
@@ -19,12 +22,21 @@ export const Container = styled.div`
 `;
 
 export const Content = styled.div`
-  width: calc(90vw - 20px);
-  padding: 5px;
+  width: 100%;
+  position: relative;
+`;
+
+export const Cover = styled.img`
+  width: 100%;
+  height: 100px;
+  object-fit: cover;
+  object-position: center;
 `;
 
 export const Close = styled.a`
   cursor: pointer;
+  position: absolute;
+  right: 0;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -32,4 +44,25 @@ export const Close = styled.a`
   height: 20px;
   padding: 5px;
   background-color: var(--si-magenta-01);
+`;
+
+export const Title = styled.p`
+  ${_ubuh2};
+  margin: 25px 0 0 10px;
+`;
+export const Subtitle = styled.p`
+  margin-left: 10px;
+  margin-bottom: 60px;
+`;
+export const Box = styled.div`
+  border: var(--si-background-01) solid 1px;
+  padding: 15px;
+  width: 50%;
+  border-radius: 10px;
+  margin: auto;
+`;
+export const Text = styled.p`
+  ${_ubutextlittle};
+  text-align: center;
+  margin: 4px 0;
 `;
