5705aa24e094b2d3f3aca4d23dfe92f7273c6b00
cancio <filipe.cancio@outlook.com>
2021-06-21 17:43:30 -0300

    corrreção do link

--- src/components/organisms/banners/BannerYoutube/index.tsx
+++ src/components/organisms/banners/BannerYoutube/index.tsx
@@ -16,7 +16,7 @@ const Index: React.FC = props => {
           <Content>
             Nossos professores tiveram a incrível ideia de trazer conteúdos de TI para além das aulas, disponibilizando novidades, discursões, rodas de conversa e eventos. Se inscreva no canal, acompanhe e participe das novidades de tecnologia.
           </Content>
-          <Link href="https://discord.gg/yCGUYTmV" target="_blank" rel="noreferrer">
+          <Link href="https://www.youtube.com/revistatihoje" target="_blank" rel="noreferrer">
             Clique aqui
           </Link>
         </SubContainer>
